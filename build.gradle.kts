5ac2199563d04642d8b67872b3f9f4f171514f14
renaud <rlpirou@gmail.com>
2026-07-13 19:45:03 +0200

    Fix 0.9.0-beta-175 build: jvmToolchain(21), drop JVM-25 override

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,16 +15,12 @@ repositories {
 }
 
 dependencies {
-    compileOnly(files("/../shared-libs/Typewriter-GUIExtension-0.0.1.jar"))
+    compileOnly(project(":Typewriter-OmniGUIExtension"))
     compileOnly("net.milkbowl.vault:VaultUnlockedAPI:2.16")
 }
 
 group = "btc.renaud"
-version = "0.0.1"
-
-dependencies {
-    implementation(files("../Typewriter-GUIExtension/build/libs/Typewriter-GUIExtension-0.1.0.jar"))
-}
+version = "0.0.2"
 
 typewriter {
     namespace = "btcrenaud"
@@ -32,16 +28,17 @@ typewriter {
         name = "NumericalStorage"
         shortDescription = "Create a Bank System in TypeWriter"
         description = "A comprehensive TypeWriter extension providing advanced gameplay features for Minecraft servers on Paper 1.21+. Fully compatible with the official TypeWriter engine and PlaceholderAPI."
-        engineVersion = "0.9.0-beta-174"
+        engineVersion = "0.9.0-beta-175"
         channel = com.typewritermc.moduleplugin.ReleaseChannel.BETA
         
+        dependencies {
+            dependency(namespace = "renaud", name = "GuiAndDialogs")
+        }
         paper()
     }
 }
 
-    
-
 kotlin {
-    jvmToolchain(25)
-    
+    jvmToolchain(21)
 }
+
